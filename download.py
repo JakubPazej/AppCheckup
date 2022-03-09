50d77b78ee23da74038123af021250457df7f267
Jakub <18260179@studentmail.ul.ie>
2022-03-09 19:51:36 +0000

    agent spoofing and little fixes

--- download.py
+++ download.py
@@ -1,53 +1,52 @@
 import sys
 import requests
-from bs4 import BeautifulSoup
-from colored import fg, bg, attr
 import re
 import progressbar
 import itertools
+import random
+from bs4 import BeautifulSoup
+from colored import fg, bg, attr
 
-
-base_url = 'https://apksfull.com'
+google_play_url = 'https://play.google.com/store/apps/details?id='
+home_url = 'https://apksfull.com'
 version_url = 'https://apksfull.com/version/'
 search_url = 'https://apksfull.com/search/'
 dl_url = 'https://apksfull.com/dl/'
-g_play_url = 'https://play.google.com/store/apps/details?id='
+
+#Made to fake and rotate agent lists to get around download restrictions
+user_agent_list = [
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
+'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
+'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
+]
+
+user_agent = random.choice(user_agent_list)
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
+    'User-Agent': user_agent,
     'Accept': 'application/json, text/javascript, */*; q=0.01',
     'Accept-Language': 'en-US,en;q=0.5',
     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
     'Connection': 'keep-alive',
 }
 
-
 def show_internet_error():
     print(fg('red') + '\n[!] ERROR: ' + attr('reset') +
           'Check your internet connection and try again.\n')
-    exit()
-
 
 def show_arg_error():
     print(fg('red') + '\n[!] ERROR: ' + attr('reset') +
           'Invalid Format\nShould be of the format `python main.py {{PACKAGE_ID}}`')
-    exit()
 
-
+#When user specifies invalid id to download the APK from
 def show_invalid_id_err():
     print('%s PackageId is invalid %s' %
           (fg('red'), attr('reset')))
-    exit()
 
-
-def make_spinner():
-    spinner = itertools.cycle(['-', '/', '|', '\\'])
-    while True:
-        sys.stdout.write(next(spinner))   # write the next character
-        sys.stdout.flush()                # flush stdout buffer (actual
-        sys.stdout.write('\b')            # erase the last written char
-
-
+#Progress bar to visualize downloading in shell
 def make_progress_bar():
     return progressbar.ProgressBar(
         redirect_stdout=True,
@@ -62,75 +61,73 @@ def make_progress_bar():
             ') ',
         ])
 
-
 def main():
-    # Get the argument from the command line
+    #Get the argument from the command line
     if len(sys.argv) != 2:
         show_arg_error()
 
-    print('%sHello, Welcome to APK Downloader 🙌 !!! %s' %
+    print('%sDownload script starting up%s' %
           (fg('cornflower_blue'), attr('reset')))
 
-    # take the package_id from the user
+    #Take the package_id from the user
     package_id = sys.argv[1]
 
     print('%sGetting download link... %s' %
           (fg('light_yellow'), attr('reset')))
 
-    # verify g_play_url with packageId string
-    g_play_res = requests.get(g_play_url + package_id,
+    #Verify google_play_url with packageId string
+    g_play_res = requests.get(google_play_url + package_id,
                               headers=headers, allow_redirects=True)
     if(g_play_res.status_code != 200):
         show_invalid_id_err()
 
-    # search the web page using the package id
+    #Search the web page using the package id
     search_res = requests.get(search_url + package_id,
                               headers=headers, allow_redirects=True)
 
-    # check the statuscode and verify it
+    #Check the statuscode and verify it
     if search_res.status_code != 200:
         show_internet_error()
 
-    # there will be a list of apps that show on the website
+    #There will be a list of apps that show on the website
     soup = BeautifulSoup(search_res.content, 'html.parser')
 
     tbody_children = soup.findAll('a')
 
     sub_dl_links = []
 
-    # tbody_children
     # loop through the children and get the href
     for item in tbody_children:
         # get href of the child
         link = item.get('href')
-        # if link contains "downwload"
+        # if link contains "download"
         if link.find("/download/") != -1:
             # append the link to the list
-            sub_dl_links.append(base_url+link)
+            sub_dl_links.append(home_url+link)
 
-    # establish a connection to the first link
+    #Establish a connection to the first link
     sub_dl_res = requests.get(sub_dl_links[0],
                               headers=headers, allow_redirects=True)
     if sub_dl_res.status_code != 200:
         show_internet_error()
 
-    # locate the script, get the contents
+    #Locate the script, get the contents
     script_text = BeautifulSoup(
         sub_dl_res.content, 'html.parser').findAll("script")
 
-    # find the last but one script tag
+    #Find the last but one script tag
     last_script = script_text[-2].contents[0]
 
-    # query the token from the script
+    #Query the token from the script
     token = re.findall("token','([^\']+)", last_script)[0]
 
-    # make a request to the download link
+    #Make a request to the download link
     dl_res = requests.post(dl_url, data={'token': token}, headers=headers)
     if dl_res.status_code != 200:
         show_internet_error()
     dl_res_json = dl_res.json()
 
-    # get the download_link from the json response
+    #Get the download_link from the json response
     try:
         download_link = dl_res_json['download_link']
     except KeyError:
@@ -138,11 +135,10 @@ def main():
              (fg('red'), attr('reset')))
         print(dl_res_json)
         quit()
-    # download the apk
+
+    #Download the apk
     print('%sDownloading APK%s' %
           (fg('yellow'), attr('reset')))
-
-    # TODO: replace app_name with actual app name
 
     app_name = package_id.replace("com.", "")
 
@@ -161,13 +157,11 @@ def main():
                 bar.update(dl)
         bar.finish()
     print('%sAPK Downloaded%s' %
-          (fg('green'), attr('reset')))
+          (fg('light_green'), attr('reset')))
 
     tmp="%sFile located in output/" +app_name+".apk%s"
     print( tmp %
-          (fg('yellow'), attr('reset')))
+          (fg('green'), attr('reset')))
 
-    exit()
-
-
+#Run the main
 main()
